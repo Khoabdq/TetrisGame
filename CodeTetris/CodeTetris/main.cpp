4adc27e57397c235a81f2e00d1f36f368150d451
25521304 <25521304@gm.uit.edu.vn>
2026-05-08 15:36:29 +0700

    them chuc nang cho ham removeline()

--- CodeTetris/CodeTetris/main.cpp
+++ CodeTetris/CodeTetris/main.cpp
@@ -116,7 +116,20 @@ bool canMove(int dx, int dy) {
 }
 
 /*them ham removeLine*/
-void removeLine() {};
+void removeLine() {
+    for (int i = H - 2; i >= 0; i--) {
+        bool full = true;
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] == ' ') { full = false; break; }
+        }
+        if (full) {
+            for (int k = i; k > 0; k--)
+                for (int j = 1; j < W - 1; j++)
+                    board[k][j] = board[k-1][j];
+            i++;
+        }
+    }
+}
 
 int main()
 {
