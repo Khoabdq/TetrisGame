cc011f1eacff47ceabf3eec982e7b9ed8ef59e77
25521304 <25521304@gm.uit.edu.vn>
2026-05-11 21:11:43 +0700

    sua ham removeLine()
    Sửa lại hàm removeLine() để nó trả về số dòng đã xóa trong lần đó

--- CodeForTetris/CodeForTetris/CodeForTetris.cpp
+++ CodeForTetris/CodeForTetris/CodeForTetris.cpp
@@ -118,19 +118,21 @@ bool canMove(int dx, int dy) {
     return true;
 }
 void removeLine() {
+    int count = 0;
     for (int i = H - 2; i >= 1; i--) {
         bool full = true;
         for (int j = 1; j < W - 1; j++) {
             if (board[i][j] == ' ') { full = false; break; }
         }
         if (full) {
-
             for (int k = i; k >= 1; k--)
                 for (int j = 1; j < W - 1; j++)
                     board[k][j] = board[k - 1][j];
             i++;
+            count++;
         }
     }
+    return count;
 }
 };
 bool canRotate() {
