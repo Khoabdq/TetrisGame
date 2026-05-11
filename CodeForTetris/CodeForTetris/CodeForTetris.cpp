8411695ea31634e266a4b5784137d1d8796628c1
25521304 <25521304@gm.uit.edu.vn>
2026-05-11 21:09:27 +0700

    them bien global: speed va linesCleared

--- CodeForTetris/CodeForTetris/CodeForTetris.cpp
+++ CodeForTetris/CodeForTetris/CodeForTetris.cpp
@@ -76,6 +76,8 @@ char blocks[][4][4] = {
 };
 
 int x = 4, y = 0, b = 1;
+int speed = 200;
+int linesCleared = 0;
 void gotoxy(int x, int y) {
     COORD c = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
