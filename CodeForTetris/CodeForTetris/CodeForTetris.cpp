a8d8e8914b144076cfb7c6d5a58710a6adde5760
25520976-WBH <25520976@gm.uit.edu.vn>
2026-05-13 21:31:50 +0700

    chuyen doi block thanh hinh vuong

--- CodeForTetris/CodeForTetris/CodeForTetris.cpp
+++ CodeForTetris/CodeForTetris/CodeForTetris.cpp
@@ -72,8 +72,19 @@ void initBoard() {
 void draw() {
     gotoxy(0, 0);
     for (int i = 0; i < H; i++, cout << endl)
-        for (int j = 0; j < W; j++)
-            cout << board[i][j];
+        for (int j = 0; j < W; j++) {
+            if (board[i][j] == '#') {
+                // Có thể dùng ký tự ASCII 219 (tùy font console) hoặc in 2 dấu vạch
+                cout << "##";
+            }
+            else if (board[i][j] == ' ') {
+                cout << "  "; // 2 dấu cách cho không gian rỗng
+            }
+            else {
+                // In ra hình vuông cho các khối gạch
+                cout << "[]";
+            }
+        }
 }
 bool canMove(int dx, int dy) {
     for (int i = 0; i < 4; i++)
@@ -138,8 +149,60 @@ void rotateBlock() {
         for (int j = 0; j < 4; j++)
             blocks[b][i][j] = temp[i][j];
 }
+int showMenu() {
+    int selectedItem = 0; // 0 là Play, 1 là Exit
+
+    while (true) {
+        system("cls"); // Xóa màn hình
+
+        cout << "====================================\n";
+        cout << "             TETRIS GAME            \n";
+        cout << "====================================\n\n";
+
+        // Hiển thị nút Play
+        if (selectedItem == 0) {
+            cout << "          >>  PLAY  <<\n";
+        }
+        else {
+            cout << "              PLAY    \n";
+        }
+
+        // Hiển thị nút Exit
+        if (selectedItem == 1) {
+            cout << "          >>  EXIT  <<\n";
+        }
+        else {
+            cout << "              EXIT    \n";
+        }
+
+        cout << "\n------------------------------------\n";
+        cout << " Su dung phim 'w' (len) va 's' (xuong)\n";
+        cout << " Nhan 'Enter' de chon.\n";
+
+        char c = _getch(); // Nhận phím từ người dùng
+
+        if (c == 'w' || c == 'W') {
+            selectedItem--;
+            if (selectedItem < 0) selectedItem = 1; // Cuộn từ trên xuống dưới
+        }
+        else if (c == 's' || c == 'S') {
+            selectedItem++;
+            if (selectedItem > 1) selectedItem = 0; // Cuộn từ dưới lên trên
+        }
+        else if (c == '\r') { // Phím Enter trong console thường có mã '\r' (Carriage Return)
+            return selectedItem;
+        }
+    }
+}
+
 int main()
 {
+    int menuChoice = showMenu();
+    if (menuChoice == 1) {
+        system("cls");
+        cout << "Cam on ban da choi game. Hen gap lai!\n";
+        return 0; // Thoát chương trình nếu chọn Exit
+    }
     srand(time(0));
     b = rand() % 7;
     system("cls");
